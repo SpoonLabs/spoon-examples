7aa2db0335ea99fc48141fd68a25a17c4ffc764b
Martin Monperrus <martin.monperrus@gnieh.org>
2015-11-21 14:02:42 +0100

    bumps to 4.4.0

--- src/main/java/fr/inria/gforge/spoon/bound/processing/BoundProcessor.java
+++ src/main/java/fr/inria/gforge/spoon/bound/processing/BoundProcessor.java
@@ -1,10 +1,13 @@
 package fr.inria.gforge.spoon.bound.processing;
 
-import fr.inria.gforge.spoon.bound.annotation.Bound;
 import spoon.processing.AbstractAnnotationProcessor;
+import spoon.reflect.code.CtCodeSnippetExpression;
+import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtIf;
+import spoon.reflect.code.CtThrow;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtParameter;
+import fr.inria.gforge.spoon.bound.annotation.Bound;
 
 /**
  * we only process method parameters (CtParameter) annotated with @Bound
@@ -18,7 +21,9 @@ public class BoundProcessor extends AbstractAnnotationProcessor<Bound, CtParamet
 		// Build if check for min.
 		CtIf anIf = getFactory().Core().createIf();
 		anIf.setCondition(getFactory().Code().<Boolean>createCodeSnippetExpression(element.getSimpleName() + " < " + annotation.min()));
-		anIf.setThenStatement(getFactory().Code().createCtThrow("new RuntimeException(\"out of min bound (\" + " + element.getSimpleName() + " + \" < " + annotation.min() + "\")"));
+		CtThrow throwStmt = getFactory().Core().createThrow();
+		throwStmt.setThrownExpression((CtExpression<? extends Throwable>) getFactory().Core().createCodeSnippetExpression().setValue("new RuntimeException(\"out of min bound (\" + " + element.getSimpleName() + " + \" < " + annotation.min() + "\")"));
+		anIf.setThenStatement(throwStmt);
 		parent.getBody().insertBegin(anIf);
 		anIf.setParent(parent);
 
